5d146df54e57d2fb79a90bf4d597f266797124ed
Matthew Higgs <m@higgs.ac>
2022-06-22 16:02:10 +0100

    Hello pip install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+from tweelytics import __version__
+
+setup(
+    name='tweelytics',
+    version=__version__,
+
+    url='https://github.com/TheLeanAcademic/tweelytics.git',
+    author='TheLeanAcademic',
+
+    py_modules=['tweelytics'],
+)
